070f37fb0c26cce131548e32d7ada24835300514
ryan campbell <ryan@thinkspace-design.com>
2013-07-04 22:44:10 -0600

    fixed functionality on in application.html.erb

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= rewuire boostrap
+//= require bootstrap
 //= require_tree .
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,6 @@ Beer::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   # in production host should be set to the actual host of you application
 end
